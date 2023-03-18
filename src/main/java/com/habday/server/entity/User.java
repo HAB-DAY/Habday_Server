01e5aacdf639039334e50fa4068431b3b2d1eb92
abi-hong <flffjakfwm20@gmail.com>
2023-03-18 18:10:55 +0900

    [init] RDS 연결

---
+++ src/main/java/com/habday/server/entity/User.java
@@ -0,0 +1,16 @@
+package com.habday.server.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+}
