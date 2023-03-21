515be8d28f8b4be6b136515e0ffcac5d5410044b
abi-hong <flffjakfwm20@gmail.com>
2023-03-22 08:57:39 +0900

    ec2 배포후 API 테스트중

--- src/main/java/com/habday/server/entity/User.java
+++ src/main/java/com/habday/server/entity/User.java
@@ -1,10 +1,13 @@
 package com.habday.server.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Getter
 @Entity
 public class User {
 
