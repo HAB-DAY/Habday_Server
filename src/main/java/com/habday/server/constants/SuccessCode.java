4509c3295574abcaa08ae03129b262b449b6eb17
yeonank <yeonank99@gmail.com>
2023-04-30 22:30:14 +0900

    [feat]예약결제 웹훅 검증 개발

--- src/main/java/com/habday/server/constants/SuccessCode.java
+++ src/main/java/com/habday/server/constants/SuccessCode.java
@@ -1,4 +1,16 @@
 package com.habday.server.constants;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Getter
+@AllArgsConstructor
 public enum SuccessCode {
+    CREATE_BILLING_KEY_SUCCESS(OK, "빌링키 발급 성공");
+
+    private final HttpStatus status;
+    private final String msg;
 }
