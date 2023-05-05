a9c8fc3e9117da480706e042e9bbf98143788d5a
abi-hong <flffjakfwm20@gmail.com>
2023-05-05 16:52:22 +0900

    [feat] 펀딩 생성 api

--- src/main/java/com/habday/server/controller/MemberController.java
+++ src/main/java/com/habday/server/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.habday.server.controller;
 
+import com.habday.server.constants.CustomException;
 import com.habday.server.domain.fundingItem.FundingItemRepository;
+import com.habday.server.domain.member.Member;
 import com.habday.server.domain.member.MemberRepository;
 import com.habday.server.dto.MemberProfileRequestDto;
 import com.habday.server.dto.MemberProfileResponse;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import static com.habday.server.constants.ExceptionCode.NO_MEMBER_ID;
 import static com.habday.server.constants.SuccessCode.CREATE_FUNDING_ITEM_SUCCESS;
 import static com.habday.server.constants.SuccessCode.VERIFY_MEMBER_PROFILE_SUCCESS;
 
@@ -26,9 +29,12 @@ public class MemberController {
         return MemberProfileResponse.newResponse(VERIFY_MEMBER_PROFILE_SUCCESS);
     }
 
-    @PostMapping("/create/fundingItem")
-    public ResponseEntity<CreateFundingItemResponse> createFundingItem(@RequestBody CreateFundingItemRequestDto request) {
-        fundingItemRepository.save(request.toCreateFundingItem(request.getFundingItemImg(), request.getFundingName(), request.getFundDetail(), request.getItemPrice(), request.getTotalPrice(), request.getGoalPrice(), request.getStartDate(), request.getFinishDate()));
+    @PostMapping("/create/fundingItem/{memberId}")
+    public ResponseEntity<CreateFundingItemResponse> createFundingItem(@PathVariable("memberId") Long memberId, @RequestBody CreateFundingItemRequestDto request) {
+
+        Member member = memberRepository.findById(memberId)
+                        .orElseThrow(() -> new CustomException(NO_MEMBER_ID));
+        fundingItemRepository.save(request.toCreateFundingItem(request.getFundingItemImg(), request.getFundingName(), request.getFundDetail(), request.getItemPrice(), request.getTotalPrice(), request.getGoalPrice(), request.getStartDate(), request.getFinishDate(), member));
         return CreateFundingItemResponse.newResponse(CREATE_FUNDING_ITEM_SUCCESS);
     }
 
