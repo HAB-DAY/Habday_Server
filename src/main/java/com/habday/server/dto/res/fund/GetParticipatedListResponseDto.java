1f53d9bc875463ee961380e43ae973d02240f35f
yeonank <yeonank99@gmail.com>
2023-06-12 16:53:42 +0900

    [feat][update] 펀딩 참여 리스트 반환 api 개발 + 펀딩 호스트 리스트 status 수정

--- src/main/java/com/habday/server/dto/res/fund/GetParticipatedListResponseDto.java
+++ src/main/java/com/habday/server/dto/res/fund/GetParticipatedListResponseDto.java
@@ -1,48 +1,61 @@
 package com.habday.server.dto.res.fund;
 
 import com.habday.server.constants.FundingState;
+import com.habday.server.constants.ScheduledPayState;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class GetParticipatedListResponseDto {
-    private FailList failList;
-    private SuccessList successList;
-    private ProgressList progressList;
+    private List<ParticipatedListInterface> participatedLists;
+    private Boolean hasNext;
+
+    public GetParticipatedListResponseDto(List<ParticipatedListInterface> participatedLists, Boolean hasNext){
+        this.participatedLists = participatedLists;
+        this.hasNext = hasNext;
+    }
     @Getter
-    public static class FailList{
+    public static class ParticipatedList{
         private Long id; //FundingItem
         private String fundingItemImg; //FundingItem
         private String fundingName; //FundingItem
-        private BigDecimal amount; //FundingMember
+        private BigDecimal totalPrice; //FundingItem
         private LocalDate startDate; //FundingItem
         private LocalDate finishDate; //FundingItem
         private FundingState status; //FundingItem
+        private LocalDate fundingDate; //FundingMember
+        private ScheduledPayState payment_status; //FundingMember
+        private String merchantId; //FundingMember
+
+        public ParticipatedList(Long id, String fundingItemImg, String fundingName,
+                           BigDecimal totalPrice, LocalDate startDate, LocalDate finishDate,
+                           FundingState status, LocalDate fundingDate, ScheduledPayState payment_status){
+            this.id = id;
+            this.fundingItemImg = fundingItemImg;
+            this.fundingName = fundingName;
+            this.totalPrice = totalPrice;
+            this.startDate = startDate;
+            this.finishDate = finishDate;
+            this.status = status;
+            this.fundingDate = fundingDate;
+            this.payment_status = payment_status;
+        }
     }
 
-    @Getter
-    public static class SuccessList{
-        private Long fundingItemId;
-        private String fundingItemImg;
-        private String fundingName;
-        private BigDecimal amount; //fundingMember에서
-        private LocalDate startDate;
-        private LocalDate finishDate;
-        private FundingState status;
-    }
-
-    @Getter
-    public static class ProgressList{
-        private Long fundingItemId;
-        private String fundingItemImg;
-        private String fundingName;
-        private BigDecimal amount; //fundingMember에서
-        private LocalDate startDate;
-        private LocalDate finishDate;
-        private FundingState status;
+    public interface ParticipatedListInterface{
+        Long getFundingMemberId(); //FundingMember
+        String getFundingName(); //FundingItem
+        BigDecimal getTotalPrice(); //FundingItem
+        LocalDate getStartDate(); //FundingItem
+        LocalDate getFinishDate(); //FundingItem
+        FundingState getStatus(); //FundingItem
+        LocalDate getFundingDate(); //FundingMember
+        ScheduledPayState getPayment_status(); //FundingMember
+        String getMerchantId(); //FundingMember
     }
 }
