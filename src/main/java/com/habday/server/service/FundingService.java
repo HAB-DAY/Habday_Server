825e574deba4d7af1e77ea05f9eba10fb9fddd37
abi-hong <flffjakfwm20@gmail.com>
2023-07-02 16:10:54 +0900

    [feat] 펀딩 목표 퍼센트 달성 여부 개발

--- src/main/java/com/habday/server/service/FundingService.java
+++ src/main/java/com/habday/server/service/FundingService.java
@@ -182,15 +182,10 @@ public class FundingService {
 
     // 펀딩 기간 만료 후, 펀딩 목표 퍼센트 달성 했는지 여부 확인 로직
     public void checkFundingFinishDate(FundingItem fundingItem){
-        System.out.println("getFinishDate^^ " + fundingItem.getFinishDate());
-
         LocalDate now = LocalDate.now(); //현재 날짜 구하기
         System.out.println("now^^ " + now.isEqual(fundingItem.getFinishDate()));
-        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd"); // 포맷정의
-        //String formatNow = now.format(formatter); //현재 날짜 포맷 적용
 
-        if (now.isEqual(fundingItem.getFinishDate())) {
-        //if (formatNow == fundingItem.getFinishDate()) { //현재날짜가 펀딩 종료 날짜일 경우
+        if (now.isEqual(fundingItem.getFinishDate())) { // 현재날짜가 펀딩 종료 날짜일 경우
             checkFundingGoalPercent(fundingItem.getItemPrice(), fundingItem.getTotalPrice(), fundingItem.getGoalPrice());
             //status SUCCESS로 바꾸기
         } else {
@@ -202,7 +197,10 @@ public class FundingService {
         BigDecimal goalPercent = goalPrice.divide(itemPrice, BigDecimal.ROUND_DOWN); //최소목표퍼센트
         BigDecimal realPercent = totalPrice.divide(itemPrice, BigDecimal.ROUND_DOWN); // 실제달성퍼센트
 
-        if (goalPercent.compareTo(realPercent) == 0 ||  goalPercent.compareTo(realPercent) == 1) {
+        System.out.println("goalPercent^^ " + goalPercent);
+        System.out.println("realPercent^^ " + realPercent);
+        System.out.println("realPercent.compareTo(goalPercent)^^ : " + realPercent.compareTo(goalPercent));
+        if (realPercent.compareTo(goalPercent) == 0 ||  realPercent.compareTo(goalPercent) == 1) { // 펀딩 최소 목표 퍼센트에 달성함
             System.out.println("최소 목표퍼센트 이상 달성함");
         } else { // 펀딩 최소 목표 퍼센트에 달성 못함
             throw new CustomException(FAIL_FINISH_FUNDING);
