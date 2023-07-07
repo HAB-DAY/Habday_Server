06903bbd638307385643f4d7614e3d769e66eab0
yeonank <yeonank99@gmail.com>
2023-07-07 10:21:55 +0900

    [feat] 이메일 보내기 기능 추가

---
+++ src/main/java/com/habday/server/config/email/EmailService.java
@@ -0,0 +1,36 @@
+package com.habday.server.config.email;
+
+import com.habday.server.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import static com.habday.server.constants.ExceptionCode.FAIL_SENDING_MAIL;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public Boolean sendEmail(EmailMessage emailMessage){
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(emailMessage.getTo()); // 메일 수신자
+            mimeMessageHelper.setSubject(emailMessage.getSubject()); // 메일 제목
+            mimeMessageHelper.setText(emailMessage.getMessage(), true); // 메일 본문 내용, HTML 여부
+            javaMailSender.send(mimeMessage);
+            log.info("Success");
+            return true;
+        }catch (MessagingException e){
+            log.info("fail");
+            throw new CustomException(FAIL_SENDING_MAIL);
+        }
+    }
+}
