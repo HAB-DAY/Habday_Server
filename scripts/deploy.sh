ab07728f398e7c8cd6dad9ddcad1d2fa2881d569
yeonank <yeonank99@gmail.com>
2023-07-09 20:13:55 +0900

    [chore] 포트 주석 지우기, deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,17 +10,20 @@ DEPLOY_PATH=/home/ec2-user/action/
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
 CURRENT_PID=$(pgrep -f $WAR_NAME)
 
-echo ">pid: $CURRENT_PID"
+echo ">pid: $CURRENT_PID" >> /home/ec2-user/action/deploy.log
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID" >> /home/ec2-user/action/deploy.log
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$WAR_NAME
-echo "> DEPLOY_JAR : $DEPLOY_JAR"
+echo "> DEPLOY_JAR : $DEPLOY_JAR" >> /home/ec2-user/action/deploy.log
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
 nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+
+
+echo "===============================" >> /home/ec2-user/action/deploy.log
