7b740f44439cfabbb9ab97e1a8f03b8ac6a1811a
abi-hong <flffjakfwm20@gmail.com>
2023-07-22 17:43:15 +0900

    [feat] accessToken 500에러 해결

--- src/main/java/com/habday/server/domain/member/MemberRepository.java
+++ src/main/java/com/habday/server/domain/member/MemberRepository.java
@@ -2,6 +2,7 @@ package com.habday.server.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,6 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByNickName(String nickName);
 
     @Query("select m from Member m where m.nickName = :nickName")
-    Member findByNickNameNoOptional(String nickName);
+    Member findByNickNameNoOptional(@Param("nickName") String nickName);
 
 }
