33e95a6d905044bc507d4fde98f00088c71957a5
yeonank <yeonank99@gmail.com>
2023-07-27 18:35:42 +0900

    [chore] 예약 시간 변경

--- src/main/java/com/habday/server/constants/CmnConst.java
+++ src/main/java/com/habday/server/constants/CmnConst.java
@@ -4,7 +4,7 @@ public class CmnConst {
     public static String server = "http://13.124.209.40:8080/";
     public static String localhost = "http://localhost:9000/";
     //TODO 스케쥴 시간 등 중요한 정보 넣기
-    public static int paymentDelayMin = 1080;//분 단위
+    public static int paymentDelayMin = 30;//분 단위
     public static int confirmLimitDate = 14; //일 단위
     public static final String scheduleCron = "0 5 0 * * *"; //"0 5 0 * * *"
 }
